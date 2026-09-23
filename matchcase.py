7cfbbe31078f0afee415a33ff1fd3b75185c70e6
unnati-398 <soniunnati500@gmail.com>
2026-09-23 09:05:09 -0700

    Add files via upload

---
+++ matchcase.py
@@ -0,0 +1,20 @@
+month = input("Enter month name:")
+match month:
+    case "january":
+        print("you have choosen january")
+    case "february":
+        print("you have choosen february")
+    case "march":
+        print("you have choosen march")
+    case "april":
+        print("you have choosen april")
+    case _:
+        print("invalid input")
+
+                
+                
+
+    
+
+        
+
